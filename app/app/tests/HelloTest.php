7761f0b90070817f5f3d4411f65d7f986a14a209
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-09-23 22:13:11 -0400

    - starting to separate services into their own respective folders - starting phpunit tests

---
+++ app/app/tests/HelloTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class HelloTest extends \PHPUnit_Framework_testCase
+{
+  public function testHello()
+  {
+    $this->assertTrue(true, "Hello, world!");
+  }
+}
---
+++ db/app/db.sql
@@ -0,0 +1,2 @@
+# creating the database
+CREATE DATABASE location;
