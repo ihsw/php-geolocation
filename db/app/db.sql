b43d0b86ddeaa47c9151f978426514a653f4d90d
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-09-23 22:35:38 -0400

    - for reference later

--- db/app/db.sql
+++ db/app/db.sql
@@ -1,2 +1,12 @@
 # creating the database
-CREATE DATABASE location;
+CREATE DATABASE stuff;
+
+# loading postgis into it
+\c stuff;
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
+CREATE EXTENSION fuzzystrmatch;
+CREATE EXTENSION postgis_tiger_geocoder;
+
+# creating the related tables
+CREATE TABLE Heroes (id INT NOT NULL, location GEOGRAPHY(POINT, 4326));
