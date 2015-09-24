722c20e38b6e67426c5de09728819f2a96c25449
Adrian Parker <iamhiddensomewhere@gmail.com>
2015-09-23 22:37:16 -0400

    - putting in proper autoincrementing

--- db/app/db.sql
+++ db/app/db.sql
@@ -9,4 +9,7 @@ CREATE EXTENSION fuzzystrmatch;
 CREATE EXTENSION postgis_tiger_geocoder;
 
 # creating the related tables
-CREATE TABLE Heroes (id INT NOT NULL, location GEOGRAPHY(POINT, 4326));
+CREATE TABLE Heroes (id SERIAL, location GEOGRAPHY(POINT, 4326));
+
+# inserting
+INSERT INTO Heroes (location) VALUES (ST_GeographyFromText('SRID=4326;POINT(45.4128061 -75.6860167)'));
